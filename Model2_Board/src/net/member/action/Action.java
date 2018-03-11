3c3fe49984500f6d7883ea4125040cb7d9a4cda5
frostnight <frostnightsong@gmail.com>
2018-03-11 19:50:10 +0900

    2018.03.11

---
+++ Model2_Board/src/net/member/action/Action.java
@@ -0,0 +1,11 @@
+package net.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.board.action.ActionForward;
+
+public interface Action {
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+		throws Exception;
+}
---
+++ Model2_Board/src/net/member/db/MemberBean.java
@@ -0,0 +1,49 @@
+package net.member.db;
+
+public class MemberBean {
+
+	private String MEMBER_ID;
+	private String MEMBER_PW;
+	private String MEMEBER_NAME;
+	private int MEMBER_AGE;
+	private String MEMBER_GENDER;
+	private String MEMBER_EMAIL;
+	
+	public String getMEMBER_ID() {
+		return MEMBER_ID;
+	}
+	public void setMEMBER_ID(String MEMBER_ID) {
+		this.MEMBER_ID = MEMBER_ID;
+	}
+	public String getMEMBER_PW() {
+		return MEMBER_PW;
+	}
+	public void setMEMBER_PW(String MEMBER_PW) {
+		this.MEMBER_PW = MEMBER_PW;
+	}
+	public String getMEMEBER_NAME() {
+		return MEMEBER_NAME;
+	}
+	public void setMEMEBER_NAME(String MEMEBER_NAME) {
+		this.MEMEBER_NAME = MEMEBER_NAME;
+	}
+	public int getMEMBER_AGE() {
+		return MEMBER_AGE;
+	}
+	public void setMEMBER_AGE(int MEMBER_AGE) {
+		this.MEMBER_AGE = MEMBER_AGE;
+	}
+	public String getMEMBER_GENDER() {
+		return MEMBER_GENDER;
+	}
+	public void setMEMBER_GENDER(String MEMBER_GENDER) {
+		this.MEMBER_GENDER = MEMBER_GENDER;
+	}
+	public String getMEMBER_EMAIL() {
+		return MEMBER_EMAIL;
+	}
+	public void setMEMBER_EMAIL(String MEMBER_EMAIL) {
+		this.MEMBER_EMAIL = MEMBER_EMAIL;
+	}
+	
+}
